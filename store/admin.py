e8825ce4ac31bee8b235bb5e050ffbe19b719b2a
Paweł Malinoś <pawel_malinos@o2.pl>
2023-02-19 15:26:52 +0100

    Add admin, models features and update requirements.txt

--- store/admin.py
+++ store/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from store.models import Product, Category, Subcategory
 
 # Register your models here.
+
+admin.site.register(Product)
+admin.site.register(Category)
+admin.site.register(Subcategory)
