9a4bf39a80b83f9bf288b0b2a1d8b731e1108332
Krzysiek <krzysztofbytnar@gmail.com>
2023-02-19 14:58:54 +0100

    dodano viewsy, templateke do strony głównej, uruchomiono próbnie serwer

--- store/models.py
+++ store/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+
 # Create your models here.
 
 class Category(models.Model):
@@ -9,12 +10,16 @@ class Category(models.Model):
         pass
 
 
+class Subcategory(models.Model):
+    name = models.CharField(max_length=50)
+    category = models.ForeignKey('store.Category', on_delete=models.CASCADE)
+
 class Product(models.Model):
     name = models.CharField(max_length=50)
     description = models.TextField(max_length=250)
     thumbnail = models.ImageField()
     category = models.ForeignKey('Category', on_delete=models.PROTECT)
-    price = models.DecimalField()
+    price = models.DecimalField(decimal_places=2, max_digits=5)
 
 
     def __str__(self):
--- store/views.py
+++ store/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
+
+
+
 # Create your views here.
+
+def home_view(request):
+    return render(request, 'store/base.html')
