80c6ee77272248b0d33a1b9f39997c62a2bca637
Paweł Malinoś <pawel_malinos@o2.pl>
2023-02-19 21:39:50 +0100

    Add pillow support and ability to add pictures to products, changes in models.

--- store/models.py
+++ store/models.py
@@ -1,3 +1,4 @@
+import os
 from django.db import models
 
 
@@ -13,7 +14,7 @@ class Profile(models.Model):
         verbose_name_plural = "Profile"
 
     def __str__(self):
-        return f"{self.user}"
+        return f"{self.user}, {self.info}"
 
 
 class Category(models.Model):
@@ -32,20 +33,26 @@ class Subcategory(models.Model):
     category = models.ForeignKey('store.Category', on_delete=models.CASCADE)
 
     def __str__(self):
-        return f"{self.name}"
+        return f"{self.category}/{self.name}"
+
     class Meta:
         verbose_name = "Podkategoria"
         verbose_name_plural = "Podkategorie"
 
+
+def tb_media_path(instance, filename):
+    file_extension = filename[filename.find(".", -5, -1)::1]
+    return os.path.join("tb", "%i_%s%s" % (instance.id, instance.name, file_extension))
+
 class Product(models.Model):
     name = models.CharField(max_length=50)
     description = models.TextField(max_length=250)
-    thumbnail = models.ImageField()
-    category = models.ForeignKey('Category', on_delete=models.PROTECT)
-    price = models.DecimalField(decimal_places=2, max_digits=5)
+    thumbnail = models.ImageField(upload_to=tb_media_path, blank=True)
+    subcategory = models.ForeignKey('Subcategory', on_delete=models.PROTECT)
+    price = models.DecimalField(decimal_places=2, max_digits=7)
 
     def __str__(self):
-        return f"{self.name}"
+        return f"{self.name}, {self.subcategory}"
 
     class Meta:
         verbose_name = "Produkt"
