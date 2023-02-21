61fa354bc237ab50b30cd8223aae3f07ba3e0f27
Paweł Malinoś <111704750+azu-pl@users.noreply.github.com>
2023-02-22 00:36:16 +0100

    Update settings.py

--- Django_web_store_project/settings.py
+++ Django_web_store_project/settings.py
@@ -127,8 +127,8 @@ STATICFILES_DIRS = [
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-LOGIN_REDIRECT_URL = 'home'
-LOGOUT_REDIRECT_URL = 'home'
+LOGIN_REDIRECT_URL = 'store'
+LOGOUT_REDIRECT_URL = 'store'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
