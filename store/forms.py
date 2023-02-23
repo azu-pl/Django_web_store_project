ac82f700c8574a678a8632ed7bf637b5fee0ba9e
Paweł Malinoś <pawel_malinos@o2.pl>
2023-02-23 10:24:29 +0100

    Add User ability to add profile information(address, phone number, and info)

--- store/forms.py
+++ store/forms.py
@@ -1,13 +1,21 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from .models import Profile
 
 
 class ResisterUserForm(UserCreationForm):
     email = forms.EmailField(required=True)
-    address = forms.CharField(max_length=150)
 
     class Meta:
         model = User
-        fields = ['username', 'email', 'password1', 'password2', 'first_name', 'last_name', 'address']
-        labels = {'username': "Nazwa", 'password': "hasło", 'first_name': "Imię", 'last_name': "Nazwisko", 'address': "adres"}
+        fields = ['username', 'email', 'password1', 'password2', 'first_name', 'last_name']
+        labels = {'username': "Nazwa", 'password': "hasło", 'first_name': "Imię", 'last_name': "Nazwisko"}
+
+class CreateUserProfileForm(forms.ModelForm):
+
+    class Meta:
+        model = Profile
+        # fields = '__all__'
+        exclude = ['user',]
+
--- store/views.py
+++ store/views.py
@@ -1,8 +1,12 @@
+from django.http import HttpResponseRedirect
 from django.shortcuts import render, redirect, get_object_or_404
 from . import forms
 from django.contrib.auth import login, logout, authenticate
 from store.models import Profile, Category
 from django.contrib.auth.models import User
+from store.forms import CreateUserProfileForm
+from django.views.generic import CreateView
+from django.urls import reverse_lazy
 
 
 def store(request):
@@ -34,7 +38,22 @@ def sign_up_view(request):
     return render(request, 'registration/sign_up.html', {'form': form})
 
 def profile_view(request):
-    profile = get_object_or_404(Profile, user_id=request.user.pk)
+    # profile = get_object_or_404(Profile, user_id=request.user.pk)
+    try:
+        profile = Profile.objects.get(user_id=request.user.pk)
+    except Profile.DoesNotExist:
+        return redirect('profile_create')
     context = {'profile': profile}
     return render(request, 'store/profile.html', context)
+
+
+class ProfileCreateView(CreateView):
+    template_name = 'store/form.html'
+    form_class = CreateUserProfileForm
+    success_url = reverse_lazy('profile')
+
+    def form_valid(self, form):
+        # here we set default logged in user
+        form.instance.user = self.request.user
+        return super().form_valid(form)
 
