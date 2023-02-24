2f0870931850b1f6a9e86e91194fe0dcc8a8a390
Krzysiek <krzysztofbytnar@gmail.com>
2023-02-24 23:48:11 +0100

    Working cart.html template, rest files has been modified by requirements for proper functionality of cart.html

--- store/urls.py
+++ store/urls.py
@@ -4,8 +4,8 @@ from .views import ProfileCreateView
 
 urlpatterns = [
     path('', views.store, name='store'),
-    path('cart/', views.cart),
-    path('checkout/', views.checkout),
+    path('cart/', views.cart, name='cart'),
+    path('checkout/', views.checkout, name='checkout'),
     path('sign-up/', views.sign_up_view, name='sign_up'),
     path('profile/', views.profile_view, name='profile'),
     path('profile/create/', ProfileCreateView.as_view(), name='profile_create')
