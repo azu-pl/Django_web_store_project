37b3304bf864d83ce8d0bb1c9a514ce1fc500ebe
Paweł Malinoś <pawel_malinos@o2.pl>
2023-02-26 01:44:50 +0100

    Add navbar functionality, changed views to model views, add product category and subcategory details views, changes in css and templates

--- store/urls.py
+++ store/urls.py
@@ -1,16 +1,28 @@
 from django.urls import path
 from . import views
-from .views import ProfileCreateView
+from .views import ProfileCreateView, HelloView, ProfileView, StoreMainView, CartView, ProductDetailView, SignUp, \
+    CheckoutView, CategoryDetailView, SubcategoryDetailView
 
 urlpatterns = [
-    path('', views.store, name='store'),
-    path('cart/', views.cart, name='cart'),
-    path('products/', views.products_list_view, name='products_list'),
-    path('products/<int:id>/', views.products_detail, name='products_detail'),
-    path('categories/', views.categories_list_view, name='categories_list'),
+    # path('', views.store, name='store'),
+    path('', StoreMainView.as_view(), name='store'),
+    # path('hello/', HelloView.as_view()),
+    # path('cart/', views.cart, name='cart'),
+    path('cart/', CartView.as_view(), name='cart'),
+    # path('products/', views.products_list_view, name='products_list'),
+    # path('products/<int:id>/', views.products_detail, name='products_detail'),
+    path('products/<int:pk>/', ProductDetailView.as_view(), name='products_detail'),
+    # path('categories/', views.categories_list_view, name='categories_list'),
+    # path('categories/<int:id>/', views.categories_detail_view, name='categories_detail_list'),
+    path('categories/<int:pk>/', CategoryDetailView.as_view(), name='categories_detail_list'),
     path('subcategories/', views.subcategories_list_view, name='subcategories_list'),
-    path('checkout/', views.checkout, name='checkout'),
-    path('sign-up/', views.sign_up_view, name='sign_up'),
-    path('profile/', views.profile_view, name='profile'),
+    # path('subcategories/<int:id>/', views.subcategories_detail_view, name='subcategories_detail_list'),
+    path('subcategories/<int:pk>/', SubcategoryDetailView.as_view(), name='subcategories_detail_list'),
+    # path('checkout/', views.checkout, name='checkout'),
+    path('checkout/', CheckoutView.as_view(), name='checkout'),
+    # path('sign-up/', views.sign_up_view, name='sign_up'),
+    path('sign-up/', SignUp.as_view(), name='sign_up'),
+    # path('profile/', views.profile_view, name='profile'),
+    path('profile/', ProfileView.as_view(), name='profile'),
     path('profile/create/', ProfileCreateView.as_view(), name='profile_create')
 ]
--- store/views.py
+++ store/views.py
@@ -1,72 +1,194 @@
-from django.http import HttpResponseRedirect
+from django.contrib.auth.forms import AuthenticationForm, UsernameField
+from django.http import HttpResponseRedirect, Http404
 from django.shortcuts import render, redirect, get_object_or_404
+from django.views.generic.base import TemplateResponseMixin, ContextMixin, View
+
 from . import forms
 from django.contrib.auth import login, logout, authenticate
-from store.models import Profile, Category, Product
+from store.models import Profile, Category, Product, Subcategory
 from django.contrib.auth.models import User
 from store.forms import CreateUserProfileForm
-from django.views.generic import CreateView
-from django.urls import reverse_lazy
+from django.views.generic import CreateView, TemplateView, DetailView
+from django.urls import reverse_lazy, reverse
 
 
-def store(request):
-    products = Product.objects.all()
-    ctx = {'products': products}
-    return render(request, 'store/store.html', ctx)
+class BaseStoreView(TemplateView):
+    template_name = "store/base.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['categories'] = Category.objects.all()
+        return context
+
+class BaseDetailView(DetailView):
+    template_name = "store/base.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['categories'] = Category.objects.all()
+        return context
+
+class BaseCreateView(CreateView):
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['categories'] = Category.objects.all()
+        return context
 
 
-def cart(request):
-    context = {}
-    return render(request, 'store/cart.html', context)
+class StoreMainView(BaseStoreView):
+    template_name = 'store/store.html'
 
-def products_list_view(request):
-    products = Product.objects.all()
-    ctx = {'products': products}
-    return render(request, 'store/products_list_1.html', ctx)
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['products'] = Product.objects.all()
+        return context
 
 
-def products_detail(request, id):
-    product = get_object_or_404(Product, id=id)
-    ctx = {'product': product}
-    return render(request, 'store/products_detail.html', ctx)
+# def store(request):
+#     products = Product.objects.all()
+#     ctx = {'products': products}
+#     return render(request, 'store/store.html', ctx)
 
-def categories_list_view(request):
-    #categories = Category.objects.all()
-    #ctx = {'categories': categories}
+class CartView(BaseStoreView):
+    template_name = 'store/cart.html'
+
+# def cart(request):
+#     context = {}
+#     return render(request, 'store/cart.html', context)
+
+
+# def products_list_view(request):
+#     products = Product.objects.all()
+#     ctx = {'products': products}
+#     return render(request, 'store/products_list_1.html', ctx)
+
+class ProductDetailView(BaseDetailView):
+    template_name = "store/product_detail.html"
+    model = Product
+
+
+# class ProductDetailView(BaseStoreView):
+#     template_name = "store/product_detail.html"
+#
+#     def get_context_data(self, **kwargs):
+#         context = super().get_context_data(**kwargs)
+#         try:
+#             product = Product.objects.get(pk=kwargs['pk'])
+#         except Product.DoesNotExist:
+#             raise Http404("Product does not exist")
+#
+#         context['product'] = product
+#         return context
+
+# def products_detail(request, id):
+#     product = get_object_or_404(Product, id=id)
+#     ctx = {'product': product}
+#     return render(request, 'store/products_detail.html', ctx)
+
+
+# def categories_list_view(request):
+#     #categories = Category.objects.all()
+#     #ctx = {'categories': categories}
+#     pass
+
+class CategoryDetailView(BaseStoreView):
+    template_name = "store/store.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['products'] = Product.objects.filter(subcategory__category=kwargs['pk'])
+        print(context)
+        return context
+
+
+class SubcategoryDetailView(BaseStoreView):
+    template_name = "store/store.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['products'] = Product.objects.filter(subcategory_id=kwargs['pk'])
+        return context
+def categories_detail_view(request, id):
     pass
+
+
+def subcategories_detail_view(request, id):
+    pass
+
 
 def subcategories_list_view(request):
     pass
 
-def checkout(request):
-    context = {}
-    return render(request, 'store/checkout.html', context)
+
+class CheckoutView(BaseStoreView):
+    template_name = 'store/checkout.html'
 
 
-def sign_up_view(request):
-    if request.method == 'POST':
-        form = forms.ResisterUserForm(request.POST)
-        if form.is_valid():
-            user = form.save()
-            login(request, user)
-            return redirect('store')
-
-    else:
-        form = forms.ResisterUserForm()
-
-    return render(request, 'registration/sign_up.html', {'form': form})
-
-def profile_view(request):
-    # profile = get_object_or_404(Profile, user_id=request.user.pk)
-    try:
-        profile = Profile.objects.get(user_id=request.user.pk)
-    except Profile.DoesNotExist:
-        return redirect('profile_create')
-    context = {'profile': profile}
-    return render(request, 'store/profile.html', context)
+class SignUp(BaseCreateView):
+    form_class = forms.ResisterUserForm
+    success_url = reverse_lazy('store')
+    template_name = 'registration/sign_up.html'
 
 
-class ProfileCreateView(CreateView):
+# def sign_up_view(request):
+#     if request.method == 'POST':
+#         form = forms.ResisterUserForm(request.POST)
+#         if form.is_valid():
+#             user = form.save()
+#             login(request, user)
+#             return redirect('store')
+#
+#     else:
+#         form = forms.ResisterUserForm()
+#
+#     return render(request, 'registration/sign_up.html', {'form': form})
+
+# def profile_view(request):
+#     # profile = get_object_or_404(Profile, user_id=request.user.pk)
+#     try:
+#         profile = Profile.objects.get(user_id=request.user.pk)
+#     except Profile.DoesNotExist:
+#         return redirect('profile_create')
+#     context = {'profile': profile}
+#     return render(request, 'store/profile.html', context)
+
+
+class HelloView(BaseStoreView):
+    template_name = "store/hello.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['name'] = "John"
+        return context
+
+
+class ProfileView(BaseStoreView):
+    template_name = "store/profile.html"
+
+    # def get_context_data(self, **kwargs):
+    #     context = super().get_context_data(**kwargs)
+    #     # try:
+    #     #     profile = Profile.objects.get(user_id=self.request.user.pk)
+    #     #
+    #     # except Profile.DoesNotExist:
+    #     #     return redirect('profile_create')
+    #
+    #     # context['profile'] = profile
+    #     print(context)
+    #     return context
+
+    def get(self, request, *args, **kwargs):
+        # self.object = self.get_object()
+        context = self.get_context_data(**kwargs)
+        try:
+            profile = Profile.objects.get(user_id=self.request.user.pk)
+            context['profile'] = profile
+            return self.render_to_response(context)
+        except:
+            return HttpResponseRedirect(reverse("profile_create"))
+
+
+class ProfileCreateView(BaseCreateView):
     template_name = 'store/form.html'
     form_class = CreateUserProfileForm
     success_url = reverse_lazy('profile')
@@ -75,8 +197,5 @@ class ProfileCreateView(CreateView):
         # here we set default logged in user
         form.instance.user = self.request.user
         return super().form_valid(form)
-
-
-
 
 
